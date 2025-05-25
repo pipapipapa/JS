e604f95729da8e156709b305d269e1b18b37d3fd
pipapipapa <andusewp@gmail.com>
2025-05-25 14:28:51 +0300

    Перевод на русский

--- pages/sequence-detail/index.js
+++ pages/sequence-detail/index.js
@@ -28,10 +28,10 @@ export class SequenceDetailPage{
                 <div id="sequence_detail_page">
                     <p class="sequence-id">${this.sequenceData.id}</p>
                     <p><strong>${this.sequenceData.name}</strong></p>
-                    <p class="sequence_author"><em>Author: ${this.sequenceData.author || 'N/A'}</em></p>
+                    <p class="sequence_author"><em>Автор: ${this.sequenceData.author || 'N/A'}</em></p>
 
                     <div class="card detail-card">
-                        <div class="card_header">Sequence Terms</div>
+                        <div class="card_header">Члены последовательности</div>
                         <div class="card_body">
                             <p id="sequence_display" class="sequence_terms_display">
                             </p>
